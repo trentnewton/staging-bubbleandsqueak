f8bfbb6ad8dcc19dc9ff41a67cf45ecd3098fc50
Trent Newton <trent99878@gmail.com>
2020-12-19 22:14:55 +1000

    decided to inline aos

--- user/themes/bubbleandsqueak/src/assets/js/app.js
+++ user/themes/bubbleandsqueak/src/assets/js/app.js
@@ -1,5 +1,5 @@
 // import IntersectionObserver from 'intersection-observer';
-import AOS from 'aos';
+// import AOS from 'aos';
 import Widow from 'widow-js';
 // import lazySizes from 'lazysizes';
 import LazyLoad from 'vanilla-lazyload';
@@ -98,29 +98,29 @@ window.onload = function() {
 }
 // })
 
-AOS.init({
-  // Global settings:
-  // disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  // startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  // initClassName: 'aos-init', // class applied after initialization
-  // animatedClassName: 'aos-animate', // class applied on animation
-  // useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  // disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  // debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  // throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+// AOS.init({
+//   // Global settings:
+//   // disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+//   // startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+//   // initClassName: 'aos-init', // class applied after initialization
+//   // animatedClassName: 'aos-animate', // class applied on animation
+//   // useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+//   // disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+//   // debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+//   // throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
-  // // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  // offset: 120, // offset (in px) from the original trigger point
-  // delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 1000, // values from 0 to 3000, with step 50ms
-  easing: 'ease-in-out-quad', // default easing for AOS animations
-  once: true, // whether animation should happen only once - while scrolling down
-  // mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+//   // // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+//   // offset: 120, // offset (in px) from the original trigger point
+//   // delay: 0, // values from 0 to 3000, with step 50ms
+//   duration: 1000, // values from 0 to 3000, with step 50ms
+//   easing: 'ease-in-out-quad', // default easing for AOS animations
+//   once: true, // whether animation should happen only once - while scrolling down
+//   // mirror: false, // whether elements should animate out while scrolling past them
+//   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
-});
+// });
 
-window.addEventListener('load', AOS.refresh);
+// window.addEventListener('load', AOS.refresh);
 
 const lazyLoadInstance = new LazyLoad({
   elements_selector: ".lazy",
